72ccd6df3b4eb9deb5f5426a04a855fe628411c6
Luky584 <30535366+Luky584@users.noreply.github.com>
2019-03-12 12:24:42 +0100

    Add files via upload

--- S4A_NodeMCU.ino
+++ S4A_NodeMCU.ino
@@ -9,62 +9,81 @@ const char* password = "RobotTesting";
 ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
 
 String page = "";
-int LEDPin = 13;
+int FPin = 13;
+int LPin = 14;
+int RPin = 15;
+int BPin = 16;
+
+
 void setup(void) {
-  //the HTML of the web page
-  page = "<h1>Robot control</h1><p><a href=\"StateL\"><button>Left</button></a>&nbsp;<a href=\"StateF\"><button>Fwd</button></a>&nbsp<a href=\"StateR\"><button>Right</button></a></p><p>&nbsp&nbsp&nbsp&nbsp<a href=\"StateB\"><button>Backward</button></a></p>";
+        //the HTML of the web page
+        page = "<h1>Robot control</h1><p><a href=\"StateL\"><button>Left</button></a>&nbsp;<a href=\"StateF\"><button>Fwd</button></a>&nbsp<a href=\"StateR\"><button>Right</button></a></p><p>&nbsp&nbsp&nbsp&nbsp<a href=\"StateB\"><button>Backward</button></a></p>";
 
-  pinMode(FPin, OUTPUT);
-  digitalWrite(FPin, LOW);
+        pinMode(FPin, OUTPUT);
+        pinMode(RPin, OUTPUT);
+        pinMode(LPin, OUTPUT);
+        pinMode(BPin, OUTPUT);
 
-  delay(1000);
-  Serial.begin(115200);
-  WiFi.softAP(ssid, password); //begin WiFi access point
-  Serial.println("");
+        digitalWrite(FPin, LOW);
+        digitalWrite(RPin, LOW);
+        digitalWrite(LPin, LOW);
+        digitalWrite(BPin, LOW);
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Created hotspot: ");
-  Serial.print(ssid);
-  Serial.print(" Passowrd: ");
-  Serial.println(password);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.softAPIP());
+        delay(1000);
+        Serial.begin(115200);
+        WiFi.softAP(ssid, password); //begin WiFi access point
+        Serial.println("");
 
-  server.on("/", []() {
-    server.send(200, "text/html", page);
-    Serial.println("On default page");
-  });
-  server.on("/StateF", []() {
-    server.send(200, "text/html", page);
-    Serial.println("Forward");
-  });
-  server.on("/StateR", []() {
-    server.send(200, "text/html", page);
-    Serial.println("Right");
-  });
-  server.on("/StateL", []() {
-    server.send(200, "text/html", page);
-    Serial.println("Left");
-  });
-  server.on("/StateB", []() {
-    server.send(200, "text/html", page);
-    Serial.println("Backward");
-  });
-  server.on("/StateS", []() {
-    server.send(200, "text/html", page);
-    Serial.println("Stop");
-  });
+        while (WiFi.status() != WL_CONNECTED) { // Wait for connection
+                delay(500);
+                Serial.print(".");
+        }
+        Serial.println("");
+        Serial.print("Created hotspot: ");
+        Serial.print(ssid);
+        Serial.print(" Passowrd: ");
+        Serial.println(password);
+        Serial.print("IP address: ");
+        Serial.println(WiFi.softAPIP());
 
-  server.begin();
-  Serial.println("Web server started!");
+        server.on("/", []() {
+                server.send(200, "text/html", page);
+                Serial.println("On default page");
+        });
+        server.on("/StateF", []() {
+                server.send(200, "text/html", page);
+                Serial.println("Forward");
+                digitalWrite(FPin, HIGH);
+        });
+        server.on("/StateR", []() {
+                server.send(200, "text/html", page);
+                Serial.println("Right");
+                digitalWrite(FPin, HIGH);
+        });
+        server.on("/StateL", []() {
+                server.send(200, "text/html", page);
+                Serial.println("Left");
+                digitalWrite(LPin, HIGH);
+        });
+        server.on("/StateB", []() {
+                server.send(200, "text/html", page);
+                Serial.println("Backward");
+                digitalWrite(BPin, HIGH);
+        });
+        server.on("/StateS", []() {
+                server.send(200, "text/html", page);
+                Serial.println("Stop");
+                digitalWrite(FPin, LOW);
+                digitalWrite(RPin, LOW);
+                digitalWrite(LPin, LOW);
+                digitalWrite(BPin, LOW);
+        });
+
+        server.begin();
+        Serial.println("Web server started!");
 }
 
 void loop(void) {
-  server.handleClient();
-  
+        server.handleClient();
+
 }
